e68a2e0290b2dfe21f246c2664482d1845223e90
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-23 13:33:21 +0200

    Removed DisplayComponent and extracted to components project

--- src/main/java/com/arnaugarcia/ars/ui/controller/HomeController.java
+++ src/main/java/com/arnaugarcia/ars/ui/controller/HomeController.java
@@ -1,17 +1,20 @@
 package com.arnaugarcia.ars.ui.controller;
 
+import com.arnaugarcia.ars.components.display.DisplayComponent;
+import com.arnaugarcia.ars.service.domain.Display;
 import com.arnaugarcia.ars.service.service.DisplayService;
 import com.arnaugarcia.ars.ui.component.Route;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.HBox;
-import javafx.scene.shape.Rectangle;
 import net.rgielen.fxweaver.core.FxmlView;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import static java.util.stream.Collectors.toList;
 
 @Component
 @FxmlView("/views/home.fxml")
@@ -32,8 +35,18 @@ public class HomeController extends Route implements Initializable {
     }
 
     private void appendDisplays() {
-        displayService.findDisplays().forEach(display -> {
-            displays.getChildren().add(new Rectangle());
-        });
+        final List<DisplayComponent> displays = displayService.findDisplays()
+                .stream()
+                .map(this::buildDisplayComponent)
+                .collect(toList());
+        this.displays.getChildren().addAll(displays);
+    }
+
+    private DisplayComponent buildDisplayComponent(Display display) {
+        final DisplayComponent displayComponent = new DisplayComponent();
+        displayComponent.setDisplayHeight(display.getHeight());
+        displayComponent.setDisplayWidth(display.getWide());
+        displayComponent.setMainDisplay(display.getMain());
+        return displayComponent;
     }
 }
