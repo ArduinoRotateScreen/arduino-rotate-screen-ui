1cd411257aad2aa6475cef7ff1dcbb38912ed312
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-23 17:38:34 +0200

    Opening footer link in browser

--- src/main/java/com/arnaugarcia/ars/ui/controller/layouts/FooterController.java
+++ src/main/java/com/arnaugarcia/ars/ui/controller/layouts/FooterController.java
@@ -1,7 +1,25 @@
 package com.arnaugarcia.ars.ui.controller.layouts;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Controller;
 
+import java.io.IOException;
+
 @Controller
+@FxmlView("/layout/footer.fxml")
 public class FooterController {
+    @FXML
+    public Hyperlink website;
+
+    public void openWebsite() {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            String url = "https://arnaugarcia.com";
+            rt.exec("open " + url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
