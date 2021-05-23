9ef52628666e7133b242536f3d9e7dafa8132c78
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-23 10:45:05 +0200

    Started with custom display component

--- src/main/java/com/arnaugarcia/ars/ui/controller/HomeController.java
+++ src/main/java/com/arnaugarcia/ars/ui/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.arnaugarcia.ars.ui.controller;
 
+import com.arnaugarcia.ars.service.service.DisplayService;
 import com.arnaugarcia.ars.ui.component.Route;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -12,8 +17,23 @@ import java.util.ResourceBundle;
 @FxmlView("/views/home.fxml")
 public class HomeController extends Route implements Initializable {
 
+    private final DisplayService displayService;
+
+    @FXML
+    public HBox displays;
+
+    public HomeController(DisplayService displayService) {
+        this.displayService = displayService;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Hello from HomeController");
+        appendDisplays();
+    }
+
+    private void appendDisplays() {
+        displayService.findDisplays().forEach(display -> {
+            displays.getChildren().add(new Rectangle());
+        });
     }
 }
