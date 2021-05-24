4520d9a517ba96f59770043f12380fbae47ce7f2
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-24 17:38:55 +0200

    Removed unused parser

--- src/main/java/com/arnaugarcia/ars/ui/controller/DeviceController.java
+++ src/main/java/com/arnaugarcia/ars/ui/controller/DeviceController.java
@@ -3,21 +3,17 @@ package com.arnaugarcia.ars.ui.controller;
 import com.arnaugarcia.ars.service.domain.Device;
 import com.arnaugarcia.ars.service.service.DeviceService;
 import com.arnaugarcia.ars.ui.component.Route;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
-import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
+
+import static javafx.collections.FXCollections.observableArrayList;
 
 @Controller
 @FxmlView("/views/devices.fxml")
@@ -26,7 +22,7 @@ public class DeviceController extends Route implements Initializable {
     private final DeviceService deviceService;
 
     @FXML
-    private ChoiceBox deviceSelector;
+    private ChoiceBox<Device> deviceSelector;
 
     public DeviceController(DeviceService deviceService) {
         this.deviceService = deviceService;
@@ -34,11 +30,8 @@ public class DeviceController extends Route implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        final List<String> deviceList = this.deviceService.getDeviceList().
-                stream()
-                .map(Device::getPort)
-                .collect(Collectors.toList());
-        final ObservableList<String> devices = FXCollections.observableArrayList(deviceList);
-        deviceSelector.setItems(devices);
+        final List<Device> deviceList = this.deviceService.getDeviceList();
+        deviceSelector.setItems(observableArrayList(deviceList));
+        deviceSelector.getSelectionModel().selectFirst();
     }
 }
