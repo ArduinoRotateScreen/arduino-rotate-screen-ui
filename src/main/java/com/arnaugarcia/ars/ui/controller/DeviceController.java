58f5eccaacc333deb026cfd18fc31244d2ca69dc
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-24 18:39:13 +0200

    Connecting to device onSelect device list

--- src/main/java/com/arnaugarcia/ars/ui/controller/DeviceController.java
+++ src/main/java/com/arnaugarcia/ars/ui/controller/DeviceController.java
@@ -3,6 +3,10 @@ package com.arnaugarcia.ars.ui.controller;
 import com.arnaugarcia.ars.service.domain.Device;
 import com.arnaugarcia.ars.service.service.DeviceService;
 import com.arnaugarcia.ars.ui.component.Route;
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
@@ -13,6 +17,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static com.fazecast.jSerialComm.SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
 import static javafx.collections.FXCollections.observableArrayList;
 
 @Controller
@@ -24,14 +29,51 @@ public class DeviceController extends Route implements Initializable {
     @FXML
     private ChoiceBox<Device> deviceSelector;
 
+    private Device currentDevice;
+
     public DeviceController(DeviceService deviceService) {
         this.deviceService = deviceService;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadDevicesInSelector();
+        deviceSelector.getSelectionModel()
+                .selectedItemProperty()
+                .addListener(onItemSelected());
+    }
+
+    private ChangeListener<Device> onItemSelected() {
+        return (observableValue, device, t1) -> {
+            this.currentDevice = observableValue.getValue();
+            connect(this.currentDevice);
+        };
+    }
+
+    private void connect(Device device)  {
+        deviceService.attachListener(device, new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                    return;
+                final SerialPort serialPort = event.getSerialPort();
+                byte[] newData = new byte[serialPort.bytesAvailable()];
+                serialPort.readBytes(newData, newData.length);
+            }
+        });
+    }
+
+    private void appendStringInLog(String data) {
+
+    }
+
+    private void loadDevicesInSelector() {
         final List<Device> deviceList = this.deviceService.getDeviceList();
         deviceSelector.setItems(observableArrayList(deviceList));
-        deviceSelector.getSelectionModel().selectFirst();
     }
 }
