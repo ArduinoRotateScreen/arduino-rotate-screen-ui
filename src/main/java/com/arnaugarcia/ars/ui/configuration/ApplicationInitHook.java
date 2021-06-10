28db0933f3099986fafe30d3000efcd2c2b17c58
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-10 19:42:20 +0200

    Renamed UserConfigurationLoader to ApplicationInitHook and added logs

--- src/main/java/com/arnaugarcia/ars/ui/configuration/ApplicationInitHook.java
+++ src/main/java/com/arnaugarcia/ars/ui/configuration/ApplicationInitHook.java
@@ -5,28 +5,34 @@ import com.arnaugarcia.ars.service.service.DeviceService;
 import com.arnaugarcia.ars.ui.service.IOService;
 import com.arnaugarcia.ars.ui.service.UserPreferenceService;
 import com.arnaugarcia.ars.ui.service.dto.UserConfigurationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-public class UserConfigurationLoader {
+public class ApplicationInitHook {
+
+    private final Logger logger = LoggerFactory.getLogger(ApplicationInitHook.class);
 
     private final UserPreferenceService userPreferenceService;
     private final DeviceService deviceService;
     private final IOService ioService;
 
-    public UserConfigurationLoader(UserPreferenceService userPreferenceService,
-                                   DeviceService deviceService,
-                                   IOService ioService) {
+    public ApplicationInitHook(UserPreferenceService userPreferenceService,
+                               DeviceService deviceService,
+                               IOService ioService) {
         this.userPreferenceService = userPreferenceService;
         this.deviceService = deviceService;
         this.ioService = ioService;
     }
 
     @EventListener
-    public void onApplicationEvent() {
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        logger.info("Trying to stream data if device is found");
         this.userPreferenceService.findUserConfiguration()
                 .flatMap(this::findDeviceByUserConfiguration)
                 .ifPresent(ioService::startStream);
