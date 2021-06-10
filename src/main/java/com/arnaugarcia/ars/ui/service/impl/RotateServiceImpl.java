ea2c7942b48e0461c88f60773a4554eef7602cd4
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-10 19:53:18 +0200

    Implementing interface in order to rotate if needed

--- src/main/java/com/arnaugarcia/ars/ui/service/impl/RotateServiceImpl.java
+++ src/main/java/com/arnaugarcia/ars/ui/service/impl/RotateServiceImpl.java
@@ -3,14 +3,16 @@ package com.arnaugarcia.ars.ui.service.impl;
 import com.arnaugarcia.ars.service.domain.Display;
 import com.arnaugarcia.ars.service.service.DisplayService;
 import com.arnaugarcia.ars.service.service.dto.DeviceData;
+import com.arnaugarcia.ars.ui.hook.events.DeviceDataEvent;
 import com.arnaugarcia.ars.ui.service.RotateService;
 import com.arnaugarcia.ars.ui.service.dto.DeviceThreshold;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 
 import static com.arnaugarcia.ars.ui.service.dto.DeviceThreshold.Between.between;
 
 @Service
-public class RotateServiceImpl implements RotateService {
+public class RotateServiceImpl implements RotateService, ApplicationListener<DeviceDataEvent> {
 
     private Display currentDisplay;
     private DeviceThreshold threshold;
@@ -51,5 +53,10 @@ public class RotateServiceImpl implements RotateService {
     @Override
     public void setThreshold(DeviceThreshold threshold) {
         this.threshold = threshold;
+    }
+
+    @Override
+    public void onApplicationEvent(DeviceDataEvent deviceDataEvent) {
+        rotateIfNeeded(deviceDataEvent.getData());
     }
 }
