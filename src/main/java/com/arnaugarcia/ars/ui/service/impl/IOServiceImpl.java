6697de9c1c1ce9bb55afdfab10a20990ae1371d8
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-10 19:52:42 +0200

    Implemented logic for attaching listener to a device

--- src/main/java/com/arnaugarcia/ars/ui/service/impl/IOServiceImpl.java
+++ src/main/java/com/arnaugarcia/ars/ui/service/impl/IOServiceImpl.java
@@ -3,13 +3,20 @@ package com.arnaugarcia.ars.ui.service.impl;
 import com.arnaugarcia.ars.service.domain.Device;
 import com.arnaugarcia.ars.service.service.DeviceService;
 import com.arnaugarcia.ars.service.service.dto.DeviceDataListener;
+import com.arnaugarcia.ars.ui.hook.events.DeviceDataEvent;
 import com.arnaugarcia.ars.ui.service.IOService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
 @Service
 public class IOServiceImpl implements IOService {
 
+    private Optional<Device> currentDevice = empty();
     private final ApplicationContext applicationContext;
     private final DeviceService deviceService;
 
@@ -20,13 +27,17 @@ public class IOServiceImpl implements IOService {
 
     @Override
     public void startStream(Device device) {
-        this.deviceService.attachListener(device, (DeviceDataListener) deviceData -> {
-            //applicationContext.publishEvent();
-        });
+        this.deviceService.attachListener(device, streamEventsToContext());
+        this.currentDevice = of(device);
     }
 
     @Override
     public void stopStream() {
+        this.currentDevice.ifPresent(deviceService::removeListener);
+        this.currentDevice = empty();
+    }
 
+    private DeviceDataListener streamEventsToContext() {
+        return deviceData -> applicationContext.publishEvent(new DeviceDataEvent(deviceData));
     }
 }
